33b2dcbe27126db5bab1245f1ea0a812ee4f198d
amr-hc <amro7275@gmail.com>
2025-01-19 21:05:06 +0200

    handle wrong route

--- backend/src/main/java/com/elboutique/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/elboutique/backend/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -44,6 +45,12 @@ public class GlobalExceptionHandler {
         response.put("error", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
-    
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<Map<String, String>> handleGlobalException(NoResourceFoundException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", "route not found");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
 
 }
